8f21953790a97714e969c1dc0166e5ea164222e3
Samvel <manukyansamvel93@gmail.com>
2021-05-25 17:39:32 +0400

    inplement water animation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// 'use strict';
+
+// var leftBorderWidth = 1;
+
+// console.log(leftBorderWidth);
+// const age = prompt('Input your age');
+// const isConfirmedAge = confirm(`Are you sure your are ${age} years old?`);
+// alert(`Is age confirmed: ${isConfirmedAge}`);
