16c5d8d8a75b2676ae795fdda634806ca82e6f3f
Samvel <manukyansamvel93@gmail.com>
2021-05-26 23:06:15 +0400

    fix scroll bag

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// 'use strict';
-
-// var leftBorderWidth = 1;
-
-// console.log(leftBorderWidth);
-// const age = prompt('Input your age');
-// const isConfirmedAge = confirm(`Are you sure your are ${age} years old?`);
-// alert(`Is age confirmed: ${isConfirmedAge}`);
